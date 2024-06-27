fbe7e58b0c59c0196c659f6da2d5acc2cffacd51
arip995 <arindampanda2002@gmail.com>
2024-06-27 12:52:17 +0530

    fixed the loading issue

--- src/app/(maindashboard)/creator/home/page.jsx
+++ src/app/(maindashboard)/creator/home/page.jsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 async function getData() {
   const res = await new Promise(resolve =>
     setTimeout(resolve, 3000)
@@ -7,7 +9,11 @@ async function getData() {
 const Page = async ({ data }) => {
   const { panda } = await getData();
 
-  return <div className=""></div>;
+  return (
+    <div className="z-20000 text-4xl text-black">
+      Home Home Home Home
+    </div>
+  );
 };
 
 export default Page;
