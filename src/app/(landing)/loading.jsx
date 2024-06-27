42eb0fa8894342df4442e95dd727ae7ba3ebd12a
arip995 <arindampanda2002@gmail.com>
2024-06-27 22:26:21 +0530

    upgraded mantine version and avatar cpmponent and new functionality like global-error and not found fixes

--- src/app/(landing)/loading.jsx
+++
@@ -1,8 +0,0 @@
-import LayoutLoading from '@/Components/Common/Loading/LayoutLoading';
-import React from 'react';
-
-const loading = () => {
-  return <LayoutLoading />;
-};
-
-export default loading;
--- src/app/global-error.jsx
+++ src/app/global-error.jsx
@@ -68,59 +68,66 @@ const error = () => {
   ];
 
   return (
-    <main>
-      <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
-        <div className="max-w-lg mx-auto text-gray-600">
-          <div className="space-y-3 text-center">
-            <h3 className="text-indigo-600 font-semibold">
-              404 Error
-            </h3>
-            <p className="text-gray-800 text-4xl font-semibold sm:text-5xl">
-              Page not found
-            </p>
-            <p>
-              Sorry, the page you are looking for could not
-              be found or has been removed.
-            </p>
-          </div>
-          <div className="mt-12">
-            <ul className="divide-y">
-              {navigations.map((item, idx) => (
-                <li key={idx} className="flex gap-x-4 py-6">
-                  <div className="flex-none w-14 h-14 bg-indigo-50 rounded-full text-indigo-600 flex items-center justify-center">
-                    {item.icon}
-                  </div>
-                  <div className="space-y-1">
-                    <h4 className="text-gray-800 font-medium">
-                      {item.title}
-                    </h4>
-                    <p>{item.desc}</p>
-                    <a
-                      href={item.href}
-                      className="text-sm text-indigo-600 duration-150 hover:text-indigo-400 font-medium inline-flex items-center gap-x-1"
+    <html>
+      <body>
+        <main>
+          <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
+            <div className="max-w-lg mx-auto text-gray-600">
+              <div className="space-y-3 text-center">
+                <h3 className="text-indigo-600 font-semibold">
+                  404 Error
+                </h3>
+                <p className="text-gray-800 text-4xl font-semibold sm:text-5xl">
+                  Page not found
+                </p>
+                <p>
+                  Sorry, the page you are looking for could
+                  not be found or has been removed.
+                </p>
+              </div>
+              <div className="mt-12">
+                <ul className="divide-y">
+                  {navigations.map((item, idx) => (
+                    <li
+                      key={idx}
+                      className="flex gap-x-4 py-6"
                     >
-                      Learn more
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="w-5 h-5"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </a>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                      <div className="flex-none w-14 h-14 bg-indigo-50 rounded-full text-indigo-600 flex items-center justify-center">
+                        {item.icon}
+                      </div>
+                      <div className="space-y-1">
+                        <h4 className="text-gray-800 font-medium">
+                          {item.title}
+                        </h4>
+                        <p>{item.desc}</p>
+                        <a
+                          href={item.href}
+                          className="text-sm text-indigo-600 duration-150 hover:text-indigo-400 font-medium inline-flex items-center gap-x-1"
+                        >
+                          Learn more
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="w-5 h-5"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </a>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </main>
+        </main>
+      </body>
+    </html>
   );
 };
 
