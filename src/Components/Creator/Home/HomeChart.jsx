96824830588ea9d4c87f336a0a3c9a036f3880c1
arip995 <arindampanda2002@gmail.com>
2024-07-01 08:13:11 +0530

    charts issue fixed

--- src/Components/Creator/Home/HomeChart.jsx
+++ src/Components/Creator/Home/HomeChart.jsx
@@ -29,15 +29,6 @@ const HomeChart = () => {
         },
       ]}
       dataKey="date"
-      // type="gradient"
-      // gradientStops={[
-      //   { offset: 0, color: 'red.6' },
-      //   { offset: 20, color: 'orange.6' },
-      //   { offset: 40, color: 'yellow.5' },
-      //   { offset: 70, color: 'lime.5' },
-      //   { offset: 80, color: 'cyan.5' },
-      //   { offset: 100, color: 'blue.5' },
-      // ]}
       strokeWidth={2}
       curveType="natural"
       yAxisProps={{ domain: [-25, 40] }}
