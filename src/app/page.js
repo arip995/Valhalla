91cc2a2a86f787f540e545ebdf3aa100c2269b40
arip995 <arindampanda2002@gmail.com>
2024-07-04 07:47:12 +0530

    updated og image

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,14 @@
-import { Alert, Button, Notification } from '@mantine/core';
-import { notifications } from '@mantine/notifications';
-import '@mantine/notifications/styles.css';
-import Hero from '@/Components/Landing/main/ui/Hero';
-import VisualFeatures from '@/Components/Landing/main/ui/VisualFeatures';
-import Features from '@/Components/Landing/main/ui/Features';
 import CTA from '@/Components/Landing/main/ui/CTA';
-import Testimonial from '@/Components/Landing/main/ui/Testimonial';
-import Pricing from '@/Components/Landing/main/ui/Pricing';
 import FAQs from '@/Components/Landing/main/ui/FAQs';
-import '../styles/landing/main.css';
-import Navbar from '@/Components/Landing/main/ui/Navbar';
+import Features from '@/Components/Landing/main/ui/Features';
 import Footer from '@/Components/Landing/main/ui/Footer';
+import Hero from '@/Components/Landing/main/ui/Hero';
+import Navbar from '@/Components/Landing/main/ui/Navbar';
+import Pricing from '@/Components/Landing/main/ui/Pricing';
+import Testimonial from '@/Components/Landing/main/ui/Testimonial';
+import VisualFeatures from '@/Components/Landing/main/ui/VisualFeatures';
+import '@mantine/notifications/styles.css';
+import '../styles/landing/main.css';
 
 export const metadata = {
   title:
@@ -18,13 +16,15 @@ export const metadata = {
   description:
     'The all-in-one platform for your digital products and services',
   openGraph: {
-    images: ['../../public/images/main/user6.webp'],
+    images: [
+      'https://nexify-try.s3.ap-south-1.amazonaws.com/a4aa635c-bae8-4b6a-9201-fd48eb3175fd.png',
+    ],
   },
 };
 
 export default function Page() {
   return (
-    <div className="flex flex-col bg-gray-900 relative">
+    <div className="relative flex flex-col bg-gray-900">
       <div className="sticky top-0 z-50 bg-gray-900">
         <Navbar />
       </div>
@@ -39,21 +39,5 @@ export default function Page() {
         <Footer />
       </div>
     </div>
-    // <div className="w-100 bg-white min-h-[100vh] text-black">
-    //   <Button
-    //     variant="filles"
-    //     onClick={() =>
-    //       notifications.show({
-    //         variant: 'filled',
-    //         withBorder: true,
-    //         color: 'green',
-    //         title: 'Default notification',
-    //         message: 'Hey there, your code is awesome! 🤥',
-    //       })
-    //     }
-    //   >
-    //     Click
-    //   </Button>
-    // </div>
   );
 }
