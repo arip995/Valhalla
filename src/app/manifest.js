3a7ed97e658965b3fda62c15c89afb75acb0511f
arip995 <arindampanda2002@gmail.com>
2024-08-05 14:15:46 +0530

    checking that if the prod is working

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,19 +1,19 @@
-export default function manifest() {
-  return {
-    name: 'Nexify',
-    short_name: 'Nexify',
-    description:
-      'Nexify is loved by the best creators around the world',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#8F00FF',
-    icons: [
-      {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
-      },
-    ],
-  };
-}
+// export default function manifest() {
+//   return {
+//     name: 'Nexify',
+//     short_name: 'Nexify',
+//     description:
+//       'Nexify is loved by the best creators around the world',
+//     start_url: '/',
+//     display: 'standalone',
+//     background_color: '#fff',
+//     theme_color: '#8F00FF',
+//     icons: [
+//       {
+//         src: '/favicon.ico',
+//         sizes: 'any',
+//         type: 'image/x-icon',
+//       },
+//     ],
+//   };
+// }
