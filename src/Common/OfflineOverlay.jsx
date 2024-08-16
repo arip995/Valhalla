6ab788126026f83ef4c6554a169c101cde259d81
arip995 <arindampanda2002@gmail.com>
2024-08-16 08:43:06 +0530

    did to able to open devtools

--- src/Common/OfflineOverlay.jsx
+++ src/Common/OfflineOverlay.jsx
@@ -1,16 +1,18 @@
 import useOfflineStatus from '@/Utils/Hooks/useOfflineStatus';
 import { IconAlertTriangle } from '@tabler/icons-react';
-import React from 'react';
+import DisableDevtool from 'disable-devtool';
 
 const OfflineOverlay = () => {
   const isOnline = useOfflineStatus();
-
+  if (process.env.NEXT_PUBLIC_ENV !== 'DEV') {
+    DisableDevtool();
+  }
   if (isOnline) {
     return null;
   }
 
   return (
-    <div className="fixed left-0 top-0 z-[200000] flex h-dvh w-full select-none justify-center bg-[#00000070] p-4">
+    <div className="fixed left-0 top-0 z-[200000] flex h-dvh w-full select-none justify-center bg-[#00000070] p-4 font-semibold">
       <div className="flex h-max gap-2 rounded-md bg-gray-900 px-4 py-2">
         <IconAlertTriangle color="white" />
         <h1 style={{ color: 'white' }}>You are offline.</h1>
