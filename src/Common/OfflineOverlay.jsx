777dc655743ac20c0cb8d32e89c7a2fd56a879e7
arip995 <arindampanda2002@gmail.com>
2024-08-18 22:24:23 +0530

    updated offline overlay

--- src/Common/OfflineOverlay.jsx
+++ src/Common/OfflineOverlay.jsx
@@ -1,6 +1,6 @@
 import useOfflineStatus from '@/Utils/Hooks/useOfflineStatus';
 import useIsBrowser from '@/Utils/useIsBrowser';
-import { IconAlertTriangle } from '@tabler/icons-react';
+import { IconCloudOff } from '@tabler/icons-react';
 import dynamic from 'next/dynamic';
 const DisableDevtool = dynamic(
   () => import('disable-devtool'),
@@ -21,7 +21,7 @@ const OfflineOverlay = () => {
   return (
     <div className="fixed left-0 top-0 z-[200000] flex h-dvh w-full select-none justify-center bg-[#00000070] p-4 font-semibold">
       <div className="flex h-max gap-2 rounded-md bg-gray-900 px-4 py-2">
-        <IconAlertTriangle color="white" />
+        <IconCloudOff color="white" />
         <h1 style={{ color: 'white' }}>You are offline.</h1>
       </div>
     </div>
