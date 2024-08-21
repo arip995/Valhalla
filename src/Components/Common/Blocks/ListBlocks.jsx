b7adfa823da446013b658393bef76c223bd86d23
arip995 <arindampanda2002@gmail.com>
2024-08-21 12:12:59 +0530

    updated the not found and global error page

---
+++ src/Components/Common/Blocks/ListBlocks.jsx
@@ -0,0 +1,80 @@
+import { ActionIcon } from '@mantine/core';
+import {
+  IconBook,
+  IconInfoCircle,
+  IconLayoutGrid,
+} from '@tabler/icons-react';
+import React from 'react';
+
+const navigations = [
+  {
+    icon: (
+      <ActionIcon variant="light" radius="lg" size="lg">
+        <IconLayoutGrid />
+      </ActionIcon>
+    ),
+    title: 'Resources',
+    desc: 'Lorem Ipsum is simply dummy text of the printing',
+    href: 'javascript:void(0)',
+  },
+  {
+    icon: (
+      <ActionIcon variant="light" radius="lg" size="lg">
+        <IconBook />
+      </ActionIcon>
+    ),
+    title: 'Guides',
+    desc: 'Lorem Ipsum is simply dummy text of the printing',
+    href: 'javascript:void(0)',
+  },
+  {
+    icon: (
+      <ActionIcon variant="light" radius="lg" size="lg">
+        <IconInfoCircle />
+      </ActionIcon>
+    ),
+    title: 'Support',
+    desc: 'Lorem Ipsum is simply dummy text of the printing',
+    href: 'javascript:void(0)',
+  },
+];
+
+const ListBlocks = () => {
+  return (
+    <ul className="divide-y">
+      {navigations.map((item, idx) => (
+        <li key={idx} className="flex gap-x-4 py-6">
+          <div className="flex h-14 w-14 flex-none items-center justify-center rounded-full text-indigo-600">
+            {item.icon}
+          </div>
+          <div className="space-y-1">
+            <h4 className="font-medium text-gray-800">
+              {item.title}
+            </h4>
+            <p>{item.desc}</p>
+            <a
+              href={item.href}
+              className="inline-flex items-center gap-x-1 text-sm font-medium text-indigo-600 duration-150 hover:text-indigo-400"
+            >
+              Learn more
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ListBlocks;
---
+++ src/Components/Common/NotFoundOne/NotFoundOne.jsx
@@ -0,0 +1,41 @@
+'use client';
+import { Button } from '@mantine/core';
+import React from 'react';
+import ListBlocks from '../Blocks/ListBlocks';
+import { useRouter } from 'next/navigation';
+
+const NotFoundOne = () => {
+  const router = useRouter();
+  return (
+    <div className="mx-auto flex h-screen max-w-screen-xl items-center justify-start bg-white px-4 md:px-8">
+      <div className="mx-auto max-w-lg text-gray-600">
+        <div className="space-y-3 text-center">
+          <h3 className="font-semibold text-indigo-600">
+            404 Error
+          </h3>
+          <p className="text-4xl font-semibold text-gray-800 sm:text-5xl">
+            Page not found
+          </p>
+          <p>
+            Sorry, the page you are looking for could not be
+            found or has been removed.
+          </p>
+        </div>
+        <div className="my-8">
+          <div className="flex w-full justify-center">
+            <Button
+              onClick={() => {
+                router.push('/creator/home');
+              }}
+            >
+              Back to Homepage
+            </Button>
+          </div>
+          <ListBlocks />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundOne;
