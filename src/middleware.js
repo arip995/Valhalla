ffe78fdf73236b177aea8a884732a89514535011
arip995 <arindampanda2002@gmail.com>
2024-08-24 01:13:18 +0530

    updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,70 +1,76 @@
-// import { getCookie } from 'cookies-next';
-// import { NextResponse } from 'next/server';
+import { getCookie } from 'cookies-next';
+import { NextResponse } from 'next/server';
 
-export function middleware() {
-  // const username = getCookie('username', { req });
-  // const accessToken = getCookie('accesstoken', { req });
-  // const redirectPaths = [
-  //   { path: '/', redirect: '/home' },
-  //   { path: '/app', redirect: '/app/lc' },
-  // ];
-  // const authRequiredPaths = [
-  //   { path: '/home', redirect: '/signin' },
-  //   { path: '/transaction', redirect: '/signin' },
-  //   { path: '/billing', redirect: '/signin' },
-  //   { path: '/audience', redirect: '/signin' },
-  //   { path: '/dashboard', redirect: '/signin' },
-  //   { path: '/purchase', redirect: '/signin' },
-  //   { path: '/account', redirect: '/signin' },
-  // ];
-  // const nonCreatorAllowedPaths = [
-  //   { path: '/purchase', next: true },
-  //   { path: '/account', next: true },
-  // ];
-  // if (accessToken) {
-  //   if (!username) {
-  //     if (
-  //       !nonCreatorAllowedPaths.some(({ path }) =>
-  //         req.nextUrl.pathname.startsWith(path)
-  //       )
-  //     ) {
-  //       return NextResponse.redirect(
-  //         new URL('/purchase', req.url)
-  //       );
-  //     }
-  //   }
-  //   for (const { path, redirect } of redirectPaths) {
-  //     if (req.nextUrl.pathname === path) {
-  //       if (username) {
-  //         return NextResponse.redirect(
-  //           new URL(redirect, req.url)
-  //         );
-  //       }
-  //     }
-  //   }
-  //   if (
-  //     req.nextUrl.pathname.startsWith('/signin') ||
-  //     req.nextUrl.pathname.startsWith('/signup')
-  //   ) {
-  //     if (username) {
-  //       return NextResponse.redirect(
-  //         new URL('/home', req.url)
-  //       );
-  //     }
-  //   }
-  // } else {
-  //   for (const { path, redirect } of authRequiredPaths) {
-  //     if (req.nextUrl.pathname.startsWith(path)) {
-  //       return NextResponse.redirect(
-  //         new URL(redirect, req.url)
-  //       );
-  //     }
-  //   }
-  // }
+export function middleware(req) {
+  const username = getCookie('username', { req });
+  const accessToken = getCookie('accesstoken', { req });
+
+  const redirectPaths = [
+    { path: '/', redirect: '/home' },
+    { path: '/app', redirect: '/app/lc' },
+  ];
+
+  const authRequiredPaths = [
+    { path: '/home', redirect: '/signin' },
+    { path: '/transaction', redirect: '/signin' },
+    { path: '/billing', redirect: '/signin' },
+    { path: '/audience', redirect: '/signin' },
+    { path: '/dashboard', redirect: '/signin' },
+    { path: '/purchase', redirect: '/signin' },
+    { path: '/account', redirect: '/signin' },
+  ];
+
+  const nonCreatorAllowedPaths = [
+    { path: '/purchase', next: true },
+    { path: '/account', next: true },
+  ];
+
+  if (accessToken) {
+    if (!username) {
+      if (
+        !nonCreatorAllowedPaths.some(({ path }) =>
+          req.nextUrl.pathname.startsWith(path)
+        )
+      ) {
+        return NextResponse.redirect(
+          new URL('/purchase', req.url)
+        );
+      }
+    }
+
+    for (const { path, redirect } of redirectPaths) {
+      if (req.nextUrl.pathname === path) {
+        if (username) {
+          return NextResponse.redirect(
+            new URL(redirect, req.url)
+          );
+        }
+      }
+    }
+
+    if (
+      req.nextUrl.pathname.startsWith('/signin') ||
+      req.nextUrl.pathname.startsWith('/signup')
+    ) {
+      if (username) {
+        return NextResponse.redirect(
+          new URL('/home', req.url)
+        );
+      }
+    }
+  } else {
+    for (const { path, redirect } of authRequiredPaths) {
+      if (req.nextUrl.pathname.startsWith(path)) {
+        return NextResponse.redirect(
+          new URL(redirect, req.url)
+        );
+      }
+    }
+  }
 }
 
 export const config = {
-  // matcher: [
-  //   '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  // ],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 };
