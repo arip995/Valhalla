19c2f0084e297efe850e4ede419f5c79993ee437
arip995 <arindampanda2002@gmail.com>
2024-08-25 22:36:29 +0530

    end of testing

--- src/middleware.js
+++ src/middleware.js
@@ -85,6 +85,6 @@ export function middleware(req) {
 
 export const config = {
   matcher: [
-    // '/((?!api|_next/static|_next/image|favicon.ico|icon.png|manifest.webmanifest).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|icon.png|manifest.webmanifest).*)',
   ],
 };
