56915151a3d7cab5b5bbbc7992a6d300d7b27119
arip995 <arindampanda2002@gmail.com>
2024-09-17 21:18:21 +0530

    updated

--- src/Components/Create/Course/CreateCourse.jsx
+++ src/Components/Create/Course/CreateCourse.jsx
@@ -24,7 +24,9 @@ const CreateCourse = () => {
             <div className="flex w-full items-center justify-between gap-4 text-sm text-gray-600">
               <div className="flex items-center gap-4">
                 <CloseButton
-                  onClick={() => router.push('/app/course')}
+                  onClick={() =>
+                    router.replace('/app/course')
+                  }
                   className="transition-colors hover:bg-gray-100"
                 />
                 <Divider
