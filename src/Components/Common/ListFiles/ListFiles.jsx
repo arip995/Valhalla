dcb87a82c633cdf4207646f3e083fb7104eca032
arip995 <arindampanda2002@gmail.com>
2024-09-23 22:09:55 +0530

    fixed locked content issue

--- src/Components/Common/ListFiles/ListFiles.jsx
+++ src/Components/Common/ListFiles/ListFiles.jsx
@@ -16,89 +16,94 @@ const ListFiles = ({
   showTrash = true,
   showImagePreview = false,
 }) => {
-  const ListOfItems = useCallback(({ item }) => {
-    if (!item.type) return null;
-    const isImage =
-      !item.loading && item.type.split('/')[0] === 'image'
-        ? true
-        : false;
-    const isAudio =
-      !item.loading && item.type.split('/')[0] === 'audio'
-        ? true
-        : false;
-    const isVideo =
-      !item.loading && item.type.split('/')[0] === 'video'
-        ? true
-        : false;
-    const isLink = item.type === 'link' ? true : false;
-    return (
-      <div
-        className="list-files-content-container"
-        key={item.id || item.url || item.link || item.name}
-      >
-        <div className="list-files-image-container flex items-center gap-2">
-          {isImage ? (
-            <>
-              {showImagePreview ? (
-                <img
-                  src={item.url}
-                  color="gray"
-                  className="list-files-image h-10 w-10 object-cover"
-                  stroke={1}
-                />
-              ) : (
-                <IconPhoto
-                  color="gray"
-                  className="list-files-image"
-                  stroke={1}
-                />
-              )}
-            </>
-          ) : isVideo ? (
-            <IconVideo
-              color="gray"
-              className="list-files-image"
-              stroke={1}
-            />
-          ) : isAudio ? (
-            <IconMusic
-              color="gray"
-              className="list-files-image"
-              stroke={1}
-            />
-          ) : isLink ? (
-            <IconLink
-              color="gray"
-              className="list-files-image"
-              stroke={1}
-            />
-          ) : (
-            <IconFileText
-              color="gray"
-              className="list-files-image"
-              stroke={1}
-            />
-          )}
-          <div className="list-files-text">
-            {item.name || item.link}
-          </div>
-        </div>
-        {showTrash && (
-          <div className="list-files-text-container">
-            <div
-              className="list-files-delete"
-              onClick={() => onDelete(item.url)}
-            >
-              <IconTrash
-                className="list-files-delete-icon"
-                size={18}
+  const ListOfItems = useCallback(
+    ({ item }) => {
+      if (!item.type) return null;
+      const isImage =
+        !item.loading && item.type.split('/')[0] === 'image'
+          ? true
+          : false;
+      const isAudio =
+        !item.loading && item.type.split('/')[0] === 'audio'
+          ? true
+          : false;
+      const isVideo =
+        !item.loading && item.type.split('/')[0] === 'video'
+          ? true
+          : false;
+      const isLink = item.type === 'link' ? true : false;
+      return (
+        <div
+          className="list-files-content-container"
+          key={
+            item.id || item.url || item.link || item.name
+          }
+        >
+          <div className="list-files-image-container flex items-center gap-2">
+            {isImage ? (
+              <>
+                {showImagePreview ? (
+                  <img
+                    src={item.url}
+                    color="gray"
+                    className="list-files-image h-10 w-10 object-cover"
+                    stroke={1}
+                  />
+                ) : (
+                  <IconPhoto
+                    color="gray"
+                    className="list-files-image"
+                    stroke={1}
+                  />
+                )}
+              </>
+            ) : isVideo ? (
+              <IconVideo
+                color="gray"
+                className="list-files-image"
+                stroke={1}
               />
+            ) : isAudio ? (
+              <IconMusic
+                color="gray"
+                className="list-files-image"
+                stroke={1}
+              />
+            ) : isLink ? (
+              <IconLink
+                color="gray"
+                className="list-files-image"
+                stroke={1}
+              />
+            ) : (
+              <IconFileText
+                color="gray"
+                className="list-files-image"
+                stroke={1}
+              />
+            )}
+            <div className="list-files-text">
+              {item.name || item.link}
             </div>
           </div>
-        )}
-      </div>
-    );
-  }, []);
+          {showTrash && (
+            <div className="list-files-text-container">
+              <div
+                className="list-files-delete"
+                onClick={() => onDelete(item.url)}
+              >
+                <IconTrash
+                  className="list-files-delete-icon"
+                  size={18}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      );
+    },
+    [files]
+  );
 
   return (
     <div className="list-files-container">
