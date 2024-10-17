09fe793423308f2d03d44f62a232b9f9b4825668
arip995 <arindampanda2002@gmail.com>
2024-10-17 16:41:49 +0530

    removed pp

--- src/Components/Landing/main/ui/Features/Features.jsx
+++ src/Components/Landing/main/ui/Features/Features.jsx
@@ -76,45 +76,45 @@ const Features = () => {
       title: 'Telegram Community',
       desc: 'Transform your Telegram community into a thriving hub with our effortless management solutions!',
     },
-    {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="icon icon-tabler icons-tabler-outline icon-tabler-credit-card-pay"
-          >
-            <path
-              stroke="none"
-              d="M0 0h24v24H0z"
-              fill="none"
-            />
-            <path d="M12 19h-6a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4.5" />
-            <path d="M3 10h18" />
-            <path d="M16 19h6" />
-            <path d="M19 16l3 3l-3 3" />
-            <path d="M7.005 15h.005" />
-            <path d="M11 15h2" />
-          </svg>
-        </svg>
-      ),
-      title: 'Payment Pages',
-      desc: 'Experience seamless, secure, and swift payments with us – your gateway to hassle-free transactions!',
-    },
+    // {
+    //   icon: (
+    //     <svg
+    //       xmlns="http://www.w3.org/2000/svg"
+    //       fill="none"
+    //       viewBox="0 0 24 24"
+    //       strokeWidth={1.5}
+    //       stroke="currentColor"
+    //       className="h-6 w-6"
+    //     >
+    //       <svg
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         width="24"
+    //         height="24"
+    //         viewBox="0 0 24 24"
+    //         fill="none"
+    //         stroke="currentColor"
+    //         strokeWidth="2"
+    //         strokeLinecap="round"
+    //         strokeLinejoin="round"
+    //         className="icon icon-tabler icons-tabler-outline icon-tabler-credit-card-pay"
+    //       >
+    //         <path
+    //           stroke="none"
+    //           d="M0 0h24v24H0z"
+    //           fill="none"
+    //         />
+    //         <path d="M12 19h-6a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4.5" />
+    //         <path d="M3 10h18" />
+    //         <path d="M16 19h6" />
+    //         <path d="M19 16l3 3l-3 3" />
+    //         <path d="M7.005 15h.005" />
+    //         <path d="M11 15h2" />
+    //       </svg>
+    //     </svg>
+    //   ),
+    //   title: 'Payment Pages',
+    //   desc: 'Experience seamless, secure, and swift payments with us – your gateway to hassle-free transactions!',
+    // },
     // {
     //   icon: (
     //     <svg
