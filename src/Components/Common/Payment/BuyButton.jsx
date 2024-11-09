150d0b3a80b405fb8b726b09055d259794d62a90
arip995 <arindampanda2002@gmail.com>
2024-11-09 11:03:26 +0530

    updated

--- src/Components/Common/Payment/BuyButton.jsx
+++ src/Components/Common/Payment/BuyButton.jsx
@@ -47,7 +47,7 @@ const BuyButton = ({
             />
           </div>
         ) : (
-          <LayoutLoading overlay type="bars" />
+          <LayoutLoading overlay size="xl" />
         )}
       </>
     );
