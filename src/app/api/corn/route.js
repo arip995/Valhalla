d4df78e7fdf6acafee35a39a9a49dc761b6e5d89
arip995 <arindampanda2002@gmail.com>
2024-11-10 10:50:20 +0530

    updated corn job

--- src/app/api/corn/route.js
+++ src/app/api/corn/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req) {
   const authHeader = req.headers.get('Authorization');
-
+  console.log('Corn job called');
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json(
       { error: 'Unauthorized' },
