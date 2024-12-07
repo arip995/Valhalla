92e08285a6d5351a3fad81fa6b68f1c114270be4
arip995 <arindampanda2002@gmail.com>
2024-12-07 23:45:03 +0530

    fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,18 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'nexify-uat.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nexify-prod.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
