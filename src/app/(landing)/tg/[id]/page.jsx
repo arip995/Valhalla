48a5d91fdcd36a939f175b656e121aa8a7c3561f
arip995 <arindampanda2002@gmail.com>
2025-01-24 23:25:53 +0530

    no result

--- src/app/(landing)/tg/[id]/page.jsx
+++ src/app/(landing)/tg/[id]/page.jsx
@@ -1,7 +1,6 @@
 import ViewTelegram from '@/Components/Landing/tg/ViewTelegram';
 import ViewTelegramClient from '@/Components/Landing/tg/ViewTelegramClient';
 import { getMetaData } from '@/Utils/getMetaData';
-import { headers } from 'next/headers';
 import { notFound } from 'next/navigation';
 
 export async function generateMetadata({ params }, parent) {
@@ -92,44 +91,7 @@ export async function generateMetadata({ params }, parent) {
   };
 }
 
-// export default async function Page({ params }) {
-//   try {
-//     const { data } = await getMetaData(params.id, 'tg');
-
-//     if (
-//       !data ||
-//       !data._id ||
-//       [0, 2, 5].includes(data.status)
-//     ) {
-//       throw new Error('notFound');
-//     }
-
-//     return (
-//       <ViewTelegram data={data} productId={params.id} />
-//     );
-//   } catch (error) {
-//     if (error.message === 'notFound') {
-//       notFound();
-//     }
-
-//     console.error(error);
-//     return <ViewTelegramClient productId={params.id} />;
-//   }
-// }
-
 export default async function Page({ params }) {
-  console.log(
-    'access-control-expose-header',
-    headers().get('access-control-expose-headers')
-  );
-  console.log('connection', headers().get('connection'));
-  console.log('from', headers().get('from'));
-  console.log('host', headers().get('host'));
-  console.log('referer', headers().get('referer'));
-  console.log('trailer', headers().get('trailer'));
-  console.log('origin', headers().get('origin'));
-  console.log('location', headers().get('location'));
-
   try {
     const { data } = await getMetaData(params.id, 'tg');
 
@@ -140,32 +102,6 @@ export default async function Page({ params }) {
     ) {
       throw new Error('notFound');
     }
-
-    // if (isTelegram) {
-    //   return (
-    //     <html>
-    //       <head>
-    //         <meta
-    //           httpEquiv="refresh"
-    //           content={`0;url=https://www.nexify.club/tg/${params.id}`}
-    //         />
-    //         <script
-    //           dangerouslySetInnerHTML={{
-    //             __html: `
-    //             window.location.href = 'https://www.nexify.club/tg/${params.id}';
-    //             setTimeout(() => {
-    //               window.open('https://www.nexify.club/tg/${params.id}', '_blank');
-    //             }, 100);
-    //           `,
-    //           }}
-    //         />
-    //       </head>
-    //       <body>
-    //         <p>Redirecting to external browser...</p>
-    //       </body>
-    //     </html>
-    //   );
-    // }
 
     return (
       <ViewTelegram data={data} productId={params.id} />
