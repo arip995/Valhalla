1c6ed94bbe4fd3580d1e22ff238991b26f86a09c
arip995 <arindampanda2002@gmail.com>
2025-01-24 22:41:34 +0530

    updated

--- src/app/(landing)/tg/[id]/page.jsx
+++ src/app/(landing)/tg/[id]/page.jsx
@@ -1,6 +1,7 @@
 import ViewTelegram from '@/Components/Landing/tg/ViewTelegram';
 import ViewTelegramClient from '@/Components/Landing/tg/ViewTelegramClient';
 import { getMetaData } from '@/Utils/getMetaData';
+import { headers } from 'next/headers';
 import { notFound } from 'next/navigation';
 
 export async function generateMetadata({ params }, parent) {
@@ -91,37 +92,7 @@ export async function generateMetadata({ params }, parent) {
   };
 }
 
-export default async function Page({ params }) {
-  try {
-    const { data } = await getMetaData(params.id, 'tg');
-
-    if (
-      !data ||
-      !data._id ||
-      [0, 2, 5].includes(data.status)
-    ) {
-      throw new Error('notFound');
-    }
-
-    return (
-      <ViewTelegram data={data} productId={params.id} />
-    );
-  } catch (error) {
-    if (error.message === 'notFound') {
-      notFound();
-    }
-
-    console.error(error);
-    return <ViewTelegramClient productId={params.id} />;
-  }
-}
-
 // export default async function Page({ params }) {
-//   const userAgent = headers().get('user-agent') || '';
-//   const isTelegram = userAgent
-//     .toLowerCase()
-//     .includes('telegram');
-//   console.log(isTelegram, 'isTelegram');
 //   try {
 //     const { data } = await getMetaData(params.id, 'tg');
 
@@ -131,32 +102,6 @@ export default async function Page({ params }) {
 //       [0, 2, 5].includes(data.status)
 //     ) {
 //       throw new Error('notFound');
-//     }
-
-//     if (isTelegram) {
-//       return (
-//         <html>
-//           <head>
-//             <meta
-//               httpEquiv="refresh"
-//               content={`0;url=https://www.nexify.club/tg/${params.id}`}
-//             />
-//             <script
-//               dangerouslySetInnerHTML={{
-//                 __html: `
-//                 window.location.href = 'https://www.nexify.club/tg/${params.id}';
-//                 setTimeout(() => {
-//                   window.open('https://www.nexify.club/tg/${params.id}', '_blank');
-//                 }, 100);
-//               `,
-//               }}
-//             />
-//           </head>
-//           <body>
-//             <p>Redirecting to external browser...</p>
-//           </body>
-//         </html>
-//       );
 //     }
 
 //     return (
@@ -171,3 +116,62 @@ export default async function Page({ params }) {
 //     return <ViewTelegramClient productId={params.id} />;
 //   }
 // }
+
+export default async function Page({ params }) {
+  console.log('agent', headers().get('user-agent'));
+  console.log('telegram', headers().get('x-telegram-web'));
+  console.log('via', headers().get('via'));
+  console.log(
+    'forwarded',
+    headers().get('x-forwarded-for')
+  );
+
+  try {
+    const { data } = await getMetaData(params.id, 'tg');
+
+    if (
+      !data ||
+      !data._id ||
+      [0, 2, 5].includes(data.status)
+    ) {
+      throw new Error('notFound');
+    }
+
+    // if (isTelegram) {
+    //   return (
+    //     <html>
+    //       <head>
+    //         <meta
+    //           httpEquiv="refresh"
+    //           content={`0;url=https://www.nexify.club/tg/${params.id}`}
+    //         />
+    //         <script
+    //           dangerouslySetInnerHTML={{
+    //             __html: `
+    //             window.location.href = 'https://www.nexify.club/tg/${params.id}';
+    //             setTimeout(() => {
+    //               window.open('https://www.nexify.club/tg/${params.id}', '_blank');
+    //             }, 100);
+    //           `,
+    //           }}
+    //         />
+    //       </head>
+    //       <body>
+    //         <p>Redirecting to external browser...</p>
+    //       </body>
+    //     </html>
+    //   );
+    // }
+
+    return (
+      <ViewTelegram data={data} productId={params.id} />
+    );
+  } catch (error) {
+    if (error.message === 'notFound') {
+      notFound();
+    }
+
+    console.error(error);
+    return <ViewTelegramClient productId={params.id} />;
+  }
+}
