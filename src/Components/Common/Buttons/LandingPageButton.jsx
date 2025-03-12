5c3ee6729367863982bf4de0686efa92124a163a
arip995 <arindampanda2002@gmail.com>
2025-03-12 21:23:18 +0530

    Refactor LandingPageButton component to enable rendering and restore button functionality

--- src/Components/Common/Buttons/LandingPageButton.jsx
+++ src/Components/Common/Buttons/LandingPageButton.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable no-unused-vars */
 'use client';
+
 import useUser from '@/Utils/Hooks/useUser';
 import { IconChevronRight } from '@tabler/icons-react';
 import Link from 'next/link';
@@ -11,19 +11,18 @@ const LandingPageButton = ({ type, ...props }) => {
   const redirectTitle = user?._id ? 'Dashboard' : 'Signup';
 
   return (
-    <></>
-    // <Link href={`/${redirect}`} {...props}>
-    //   {type ? (
-    //     <button className="rounded-full bg-gradient-to-r from-purple-600 to-pink-600 px-6 py-2 text-sm font-medium text-white shadow-lg shadow-purple-500/30 hover:opacity-90">
-    //       {redirectTitle}
-    //     </button>
-    //   ) : (
-    //     <button className="flex w-full transform items-center justify-center rounded-full bg-gradient-to-r from-purple-600 to-pink-600 px-8 py-3 text-base font-medium text-white shadow-lg shadow-purple-500/30 transition-transform hover:scale-105 hover:opacity-90 sm:w-auto">
-    //       Start Free Trial
-    //       <IconChevronRight className="ml-2 h-5 w-5" />
-    //     </button>
-    //   )}
-    // </Link>
+    <Link href={`/${redirect}`} {...props}>
+      {type ? (
+        <button className="rounded-full bg-gradient-to-r from-purple-600 to-pink-600 px-6 py-2 text-sm font-medium text-white shadow-lg shadow-purple-500/30 hover:opacity-90">
+          {redirectTitle}
+        </button>
+      ) : (
+        <button className="flex w-full transform items-center justify-center rounded-full bg-gradient-to-r from-purple-600 to-pink-600 px-8 py-3 text-base font-medium text-white shadow-lg shadow-purple-500/30 transition-transform hover:scale-105 hover:opacity-90 sm:w-auto">
+          Start Free Trial
+          <IconChevronRight className="ml-2 h-5 w-5" />
+        </button>
+      )}
+    </Link>
   );
 };
 
