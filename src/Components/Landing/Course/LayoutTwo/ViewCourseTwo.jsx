b509230983b14272194b172ccba94d6bbe82a776
arip995 <arindampanda2002@gmail.com>
2025-03-19 20:35:04 +0530

    update in image size

--- src/Components/Landing/Course/LayoutTwo/ViewCourseTwo.jsx
+++ src/Components/Landing/Course/LayoutTwo/ViewCourseTwo.jsx
@@ -51,8 +51,8 @@ const ViewCourseTwo = ({ data, isPreview }) => {
                   <div className="aspect-video overflow-hidden rounded-2xl shadow-2xl">
                     <Image
                       src={data.coverImage.url}
-                      height={300}
-                      width={400}
+                      height={600}
+                      width={800}
                       className="h-full w-full object-cover transition duration-700 hover:scale-105"
                       alt={data.title}
                       priority
